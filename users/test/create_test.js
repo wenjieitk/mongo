8c7b971d1325ee5be569540940d24fab9118124c
Wen Jie <wenjie@cavman.com.my>
2018-07-27 18:32:14 +0800

    - assert if doc saved successfully - mongoose promise set up

--- users/test/create_test.js
+++ users/test/create_test.js
@@ -2,12 +2,16 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Creating records', () => {
-    it('it should saves a user', () => {
+    it('it should saves a user', (done) => {
         //create instance
         const joe = new User({
             name: 'Joe'
         });
 
-        joe.save();
+        joe.save().then(() => {
+            // has joe been saved successfully?
+            assert(!joe.isNew);
+            done();
+        });
     });
 });
--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const mongoUri ='mongodb://localhost:27017/Users_test';
 
-// mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise;
 
 mongoose.connect(mongoUri, {
     useNewUrlParser: true
