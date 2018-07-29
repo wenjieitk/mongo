9f832e15c661693d5e7a96a4bcbb21f267bdeda0
Wen Jie <wenjie@cavman.com.my>
2018-07-29 19:32:46 +0800

    skip() , limit(), sort()

--- users/test/reading_test.test.js
+++ users/test/reading_test.test.js
@@ -2,20 +2,35 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Finding users out of database', () => {
-    let joe;
+    let joe, maria,alex,zach;
 
     // insert testing data before every test
     beforeEach((done) => {
         joe = new User({
-            name: 'Joe'
+            name: 'joe'
         });
-        joe.save()
-            .then(() => done());
+        maria = new User({
+            name: 'maria'
+        });
+        alex = new User({
+            name: 'alex'
+        });
+        zach = new User({
+            name: 'zach'
+        });
+
+        Promise.all([
+            joe.save(),
+            maria.save(),
+            alex.save(),
+            zach.save()
+        ]).then(() => done());
+
     });
 
     it('finds all users wtih a name of joe', (done) => {
         User.find({
-            name: 'Joe'
+            name: 'joe'
         }).then((users) => {
             assert(users[0]._id.toHexString() === joe._id.toHexString());
             done();
@@ -26,8 +41,23 @@ describe('Finding users out of database', () => {
         User.findOne({
             _id:joe._id
         }).then((user) => {
-            assert(user.name === 'Joe');
+            assert(user.name === 'joe');
             done();
         });
+    });
+
+    it('it should skip and limit the result set', (done) => {
+        User.find({})
+            .sort({
+                name: 1
+            })
+            .skip(1)
+            .limit(2)
+            .then((users) => {
+                assert(users.length === 2);
+                assert(users[0].name === 'joe');
+                assert(users[1].name === 'maria');
+                done();
+            })
     });
 });
