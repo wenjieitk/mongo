363d8265a5c7819885af22639f9fabd831367e41
Wen Jie <wenjie@cavman.com.my>
2018-08-01 19:25:24 +0800

    code refactor

--- muber/test/app.test.js
+++ muber/test/app.test.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 const request = require('supertest');
-const app = require('../../app')
+const app = require('../app')
 
 describe('test express app', () => {
     it('it should handles a GET request to /api', (done) => {
